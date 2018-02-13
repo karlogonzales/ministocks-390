4301508ed445b2af6182ebde15e48386ac4c10e1
Andy <andy.pham2956@gmail.com>
2018-02-12 22:24:32 -0500

    second resizable to show horizontal resize

---
+++ src/main/java/nitezh/ministock/activities/configure/ConfigureActivityResizable2.java
@@ -0,0 +1,16 @@
+package nitezh.ministock.activities.configure;
+
+
+import android.os.Bundle;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ConfigureActivityResizable2 extends ConfigureActivityBase {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mWidgetSize = 3;
+        super.onCreate(savedInstanceState);
+    }}
+
