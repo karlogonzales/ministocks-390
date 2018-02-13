df469e99cf1c4e1d3bb3ce93a6b64ca694818e44
hassanahmed95 <hassan.514@hotmail.com>
2018-02-13 04:42:12 -0500

    Implemented the test that looks for the two new sizes of widgets

--- src/androidTest/java/nitzeh/ministock/ui/ResizeableWidgetTest.java
+++ src/androidTest/java/nitzeh/ministock/ui/ResizeableWidgetTest.java
@@ -7,28 +7,28 @@ import android.support.test.runner.AndroidJUnit4;
 import android.support.test.runner.AndroidJUnitRunner;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.By;
-import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
-import android.test.InstrumentationTestCase;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static junit.framework.Assert.assertNotNull;
 
+/**
+ * This class is an instrumentation test that uses the ui automator to check if
+ * the two new resizeable ministocks widgets have been added to the list of available
+ * widgets
+ */
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
 
 public class ResizeableWidgetTest extends AndroidJUnitRunner{
-    private static final String BASIC_SAMPLE_PACKAGE
-            = "com.example.android.testing.uiautomator.BasicSample";
-    private static final int LAUNCH_TIMEOUT = 5000;
-    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private static final int LAUNCH_TIMEOUT = 5000;;
     private UiDevice mDevice;
 
     @Override
@@ -37,15 +37,19 @@ public class ResizeableWidgetTest extends AndroidJUnitRunner{
     }
 
     @Test
-    public void setUp() throws InterruptedException {
+    public void searchForResizeableTest() throws InterruptedException, UiObjectNotFoundException {
+
         // Initialize UiDevice instance
         mDevice = UiDevice.getInstance(getInstrumentation());
+
+        //Get the center of the screen to be used for long press
         Point screenSize = new Point(mDevice.getDisplayWidth(), mDevice.getDisplayHeight());
         Point screenCenter = new Point(screenSize.x / 2, screenSize.y /2);
         Point segments[] = {screenCenter, screenCenter};
 
         // Start from the home screen
         mDevice.pressHome();
+        mDevice.waitForIdle();
 
         // Wait for launcher
         final String launcherPackage = mDevice.getLauncherPackageName();
@@ -57,7 +61,39 @@ public class ResizeableWidgetTest extends AndroidJUnitRunner{
         mDevice.swipe(segments, 150);
         Thread.sleep(2000);
 
+        //Bring up the list of widgets
         mDevice.findObject(By.text("Widgets")).click();
         mDevice.waitForIdle();
+        Thread.sleep(2000);
+
+        UiScrollable widgets = new UiScrollable(new UiSelector().scrollable(true));
+
+        // Set swiping mode to horizontal and scroll to the beginning of the list
+        widgets.setAsHorizontalList();
+        widgets.scrollToBeginning(50);
+        int maxSearchSwipes = widgets.getMaxSearchSwipes();
+
+        UiObject2 resizeableWidgetOne = null;
+        UiObject2 resizeableWidgetTwo = null;
+
+        // Swipe forward through the list of widgets until they have been found
+        for (int i = 0; i < maxSearchSwipes; i++){
+            if (resizeableWidgetOne == null){
+                resizeableWidgetOne = mDevice.findObject(By.text("Resizable"));
+            }
+
+            if (resizeableWidgetTwo == null){
+                resizeableWidgetTwo = mDevice.findObject(By.text("Resizable2"));
+            }
+
+            if (resizeableWidgetOne != null && resizeableWidgetTwo != null){
+                break;
+            } else{
+                widgets.scrollForward();
+            }
+        }
+
+        assertNotNull(resizeableWidgetOne);
+        assertNotNull(resizeableWidgetTwo);
     }
 }
