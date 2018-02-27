b630d11cb8b5b3ca24681c81c1ed689b1f956559
Andy <andy.pham2956@gmail.com>
2018-02-27 17:31:22 -0500

    fix display issue on the scrollable widget view #7

--- src/main/java/nitezh/ministock/activities/widget/WidgetScrollableProvider.java
+++ src/main/java/nitezh/ministock/activities/widget/WidgetScrollableProvider.java
@@ -3,7 +3,12 @@ package nitezh.ministock.activities.widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 import nitezh.ministock.R;
 
@@ -12,13 +17,15 @@ import nitezh.ministock.R;
  */
 public class WidgetScrollableProvider extends AppWidgetProvider {
 
+    ListView mListView;
+    int num = 0;
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_scrollable);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -35,11 +42,36 @@ public class WidgetScrollableProvider extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+        mListView.findViewById(R.id.list_view);
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return num;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = inflater().inflate(R.layout.scrollable_item,null);
+            return null;
+        }
+
     }
 }
 
