954b5a55ca9a065cc49543fca5b9cd8b2a8793b2
hassanahmed95 <hassan.514@hotmail.com>
2018-02-28 20:41:47 -0500

    #11: Added a function buildRequestURL that builds the properrequest URL based on the list of symbols provided.

--- src/main/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepository.java
+++ src/main/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepository.java
@@ -3,12 +3,27 @@ package nitezh.ministock.dataaccess;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.List;
+
 import nitezh.ministock.utils.Cache;
 import nitezh.ministock.utils.UrlDataTools;
 
 public class CryptoCompareQuoteRepository {
-    private static final String BASE_URL = "https://min-api.cryptocompare.com/data/";
+    private static final String BASE_URL = "https://min-api.cryptocompare.com/data/pricemulti?";
 
+    private String buildRequestUrl(List<String> symbols){
+        StringBuilder sQuery = new StringBuilder();
+
+        for (String s : symbols){
+            if (!s.equals("")){
+                if(!sQuery.toString().equals("")){
+                    sQuery.append(",");
+                }
+                sQuery.append(s);
+            }
+        }
+        return String.format("%s%s%s", BASE_URL,sQuery, "&tsyms=USD");
+    }
     public JSONArray retrieveQuotesAsJson(Cache cache) throws JSONException {
         String url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=JPY,EUR";
 
