a7acc75621ea8df450d3a58f1bb6d6a9e3fb5239
Karlo Gonzales <karlo.gonzales@gmail.com>
2018-02-28 21:43:58 -0500

    Added crypto api response test

--- src/test/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepositoryTests.java
+++ src/test/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepositoryTests.java
@@ -2,6 +2,7 @@ package nitezh.ministock.dataaccess;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import nitezh.ministock.mocks.MockCache;
+import nitezh.ministock.utils.UrlDataTools;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -28,5 +30,15 @@ public class CryptoCompareQuoteRepositoryTests {
         String expected = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD";
 
         assertEquals(expected, this.cryptoRepository.buildRequestUrl(symbols));
+    }
+
+    @Test
+    public void responseTest(){
+        MockCache mockCache1 = new MockCache();
+        MockCache mockCache2 = new MockCache();
+        List<String> symbols = Arrays.asList("BTC");
+        String responseTest = UrlDataTools.getCachedUrlData("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD",mockCache1,300);
+        assertEquals(UrlDataTools.getCachedUrlData(this.cryptoRepository.buildRequestUrl(symbols),mockCache2,300), responseTest);
+
     }
 }
