39a892189d8dfb38e03935fb728d981a627f7179
hassanahmed95 <hassan.514@hotmail.com>
2018-02-28 22:08:17 -0500

    #11: modified response test to pass with the new changes to crypto repository. Changes url to get pull stock info & used a JSONObject to compare responses because the JSON library arranges objects alphabetically

--- src/test/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepositoryTests.java
+++ src/test/java/nitezh/ministock/dataaccess/CryptoCompareQuoteRepositoryTests.java
@@ -1,5 +1,7 @@
 package nitezh.ministock.dataaccess;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -51,12 +53,11 @@ public class CryptoCompareQuoteRepositoryTests {
     }
 
     @Test
-    public void responseTest(){
+    public void responseTest() throws JSONException {
         MockCache mockCache1 = new MockCache();
-        MockCache mockCache2 = new MockCache();
         List<String> symbols = Arrays.asList("BTC");
-        String responseTest = UrlDataTools.getCachedUrlData("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD",mockCache1,300);
-        assertEquals(UrlDataTools.getCachedUrlData(this.cryptoRepository.buildRequestUrl(symbols),mockCache2,300), responseTest);
+        JSONObject responseTest = new JSONObject(UrlDataTools.getCachedUrlData("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD",mockCache1,300));
 
+        assertEquals(responseTest.toString(),cryptoRepository.retrieveQuotesAsJson(new MockCache(),symbols).toString());
     }
 }
