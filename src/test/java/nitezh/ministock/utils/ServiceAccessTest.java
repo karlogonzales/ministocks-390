4071cd45a607ef1476f48f39edf419434b1c595d
karlogonzales <karlo.gonzales@gmail.com>
2018-03-16 14:33:33 -0400

    Issue #54 - Added ServiceAccess unit Tests

--- src/test/java/nitezh/ministock/utils/ServiceAccessTest.java
+++ src/test/java/nitezh/ministock/utils/ServiceAccessTest.java
@@ -1,19 +1,33 @@
 package nitezh.ministock.utils;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.junit.Test;
+import junit.framework.TestCase;
 
-import static org.junit.Assert.*;
+import org.json.JSONObject;
+import org.junit.Assume;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+
 
 /**
- * Created by k_onzale on 15-Mar-18.
+ * Created by karlo on 2018-03-16.
  */
-public class ServiceAccessTest {
-
-    @Test
+public class ServiceAccessTest extends TestCase {
     public void testExecute() throws Exception {
-//        JSONObject response = new ServiceAccess().execute("CRYPTO", "BTC");
-//        System.out.println(response.get("CAD"));
+        ServiceAccess mockServiceAccess = mock(ServiceAccess.class);
+        Mockito.when(mockServiceAccess.execute("https:api.iextrading.com/1.0/stock/GOOG/price", "GET")).thenReturn("{1000.00}");
+        String res = mockServiceAccess.execute("https:api.iextrading.com/1.0/stock/GOOG/price", "GET");
+
+        Mockito.verify(mockServiceAccess).execute("https:api.iextrading.com/1.0/stock/GOOG/price", "GET");
+        assertEquals(res, "{1000.00}");
+    }
+
+    public void testResponse() throws Exception {
+        Assume.assumeTrue(System.getenv("TRAVIS_CI") == null);
+
+        ServiceAccess serviceAccess = new ServiceAccess();
+        String res = serviceAccess.execute("https:api.iextrading.com/1.0/stock/GOOG/price", "GET");
+
+        assertEquals(res.isEmpty(),false );
     }
 }
