536ce27532d2887ab96c8f40c397672661580a89
Lattakia <hannageorgi@hotmail.com>
2018-03-29 18:20:12 -0400

    Adding a list view to the add menu Issue#45
    the add menu now shows a scrollable list of all the values added, this list updates everytime a new value is added.

--- src/main/java/nitezh/ministock/activities/menu/MenuScrollableAdd.java
+++ src/main/java/nitezh/ministock/activities/menu/MenuScrollableAdd.java
@@ -9,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
-import nitezh.ministock.R;
+import java.util.ArrayList;
 
+import nitezh.ministock.R;
+import nitezh.ministock.utils.ListItem;
 
 
 public class MenuScrollableAdd extends Activity implements View.OnClickListener{
@@ -21,10 +24,10 @@ public class MenuScrollableAdd extends Activity implements View.OnClickListener{
     //temporary list search
     private String[] stockListSearch = {"BTC", "ETH", "GOOG", "AAPL", "TSLA", "GM", "NFLX", "DIS", "TWTR", "PYPL", "FEYE", "FB", "BABA"};
     private ArrayAdapter<String> adapter;
-
+    private ArrayList<String> myStocks;
     private View additem;
+    private ArrayAdapter<String> adapter1;
     private AutoCompleteTextView textView;
-    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,27 +45,27 @@ public class MenuScrollableAdd extends Activity implements View.OnClickListener{
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,stockListSearch);
         textView.setAdapter(adapter);
+        myStocks = new ArrayList <String>();
 
+    }
 
-
-
+    private void populateListView(){
+        adapter1 = new ArrayAdapter<String>(this,R.layout.menu_list_item,myStocks);
+        ListView listView = (ListView) findViewById(R.id.menuListView);
+        listView.setAdapter(adapter1);
     }
 
     @Override
     public void onClick(View v) {
 
-        text = (TextView)findViewById(R.id.text_test);
-        
         if(textView.getVisibility()==View.GONE){
             textView.setVisibility(View.VISIBLE);
             textView.requestFocus();
-
         }
         else{
             textView.setVisibility(View.GONE);
-            text.setText(textView.getText());
+            myStocks.add(textView.getText().toString());
+            populateListView();
         }
-
-
     }
 }
