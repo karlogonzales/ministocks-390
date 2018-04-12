55cfac49fbbc863c9fd8c8e2e9dca0b058fa803d
Andy <andy.pham2956@gmail.com>
2018-04-12 00:10:28 -0400

    main functionality of remove feature, issue #71

--- src/main/java/nitezh/ministock/activities/menu/MenuScrollableRemove.java
+++ src/main/java/nitezh/ministock/activities/menu/MenuScrollableRemove.java
@@ -5,19 +5,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
 
 import nitezh.ministock.R;
+import nitezh.ministock.utils.StockListSingleton;
 
-public class MenuScrollableRemove extends Activity{
+public class MenuScrollableRemove extends Activity implements AdapterView.OnItemClickListener{
 
     //temporary list search
     private String[] stockListSearch = {"BTC", "ETH", "GOOG", "AAPL", "TSLA", "GM", "NFLX", "DIS", "TWTR", "PYPL", "FEYE", "FB", "BABA"};
     private EditText editSearch;
     private ListView listSearch;
     private ArrayAdapter<String> adapter;
+
+    private ArrayList<String> stockList = StockListSingleton.getInstance().getData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,10 @@ public class MenuScrollableRemove extends Activity{
 
         listSearch = (ListView) findViewById(R.id.listSearch);
         editSearch = (EditText) findViewById(R.id.editSearch);
-        adapter = new ArrayAdapter<String>(this, R.layout.scroll_menu_add_search_listitem, R.id.textView, stockListSearch);
+        adapter = new ArrayAdapter<String>(this, R.layout.scroll_menu_add_search_listitem, R.id.textView, stockList);
         listSearch.setAdapter(adapter);
+
+        listSearch.setOnItemClickListener(this);
 
         editSearch.addTextChangedListener(new TextWatcher() {
             @Override
@@ -45,5 +56,19 @@ public class MenuScrollableRemove extends Activity{
 
             }
         });
+    }
+    @Override
+    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+        // Get the selected item text from ListView
+        String item = Integer.toString(position);
+
+        Toast toast= Toast.makeText(getApplicationContext(),item,Toast.LENGTH_SHORT);
+        toast.show();
+        removeStockData(position);
+    }
+
+
+    public void removeStockData(int position){
+        stockList.remove(position);
     }
 }
--- src/main/java/nitezh/ministock/utils/StockListSingleton.java
+++ src/main/java/nitezh/ministock/utils/StockListSingleton.java
@@ -9,6 +9,7 @@ public class StockListSingleton {
     private ArrayList<String> data = new ArrayList<>();
     public ArrayList<String> getData() {return data;}
     public void addData(String data) {this.data.add(data);}
+    public void deleteData(int position){this.data.remove(position);}
 
     private static final StockListSingleton stocklist = new StockListSingleton();
     public static StockListSingleton getInstance() {return stocklist;}
